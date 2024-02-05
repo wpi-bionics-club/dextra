db578614352b2a82130b33010d9e8016a89e2e8f
minaboktor2628 <148801385+minaboktor2628@users.noreply.github.com>
2024-02-05 18:02:26 -0500

    added linear control function

--- motor_control/src/main.cpp
+++ motor_control/src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
-//long timeEleapsed = millis();
 long previousTime;
 
 // put function declarations here:
+void linearControl(int startingAngle, int time);
 int myFunction(int, int);
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,27 +13,36 @@ void setup() {
 }
 
 void loop() {
+  int timePerClock = 1000;
+  int angleResolution = 10;
+  int startingAngle = 0;
+  int finalAngle = 180;
   // put your main code here, to run repeatedly:
   Serial.println("hello");
 
   while(1){
-    if(((millis() - previousTime) >= 1000) ? previousTime=millis() : 0 ){
-      //previousTime = millis();
-      //run your code
+    if(((millis() - previousTime) >= timePerClock) ? previousTime = millis() : 0 ){
+      linearControl(angleResolution, startingAngle, finalAngle, timePerClock);
       Serial.print("This is printing every second ");
       Serial.println(previousTime);
     }
   }
-
-
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
-  
-  
   millis();
-  
   return x + y;
-  
+}
+
+
+// a = delta theta / time in seconds
+void linearControl(int angleIncrimint, int startingAngle, int finalAngle, int timeIncrimint){
+  int goalTheta = startingAngle;
+
+  while (startingAngle < finalAngle){
+    goalTheta += angleIncrimint;
+    write(0, goalTheta);
+  }
+
 }
