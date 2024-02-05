b6a165fb46e65cfa027cff91d5fdda0f7691df4a
Eturas314 <144499966+Eturas314@users.noreply.github.com>
2024-02-05 17:44:10 -0500

    Able to set time, updates every second.

--- motor_control/src/main.cpp
+++ motor_control/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+//long timeEleapsed = millis();
+long previousTime;
 
 // put function declarations here:
 int myFunction(int, int);
 
+
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
+  Serial.begin(9600);
+  previousTime = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("hello");
+
+  while(1){
+    if(((millis() - previousTime) >= 1000) ? previousTime=millis() : 0 ){
+      //previousTime = millis();
+      //run your code
+      Serial.print("This is printing every second ");
+      Serial.println(previousTime);
+    }
+  }
+
+
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
+  
+  
+  millis();
+  
   return x + y;
+  
 }
