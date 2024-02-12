c3ccfc772ca04df6420d067d63f2163f464f32c3
minaboktor2628 <148801385+minaboktor2628@users.noreply.github.com>
2024-02-12 18:45:09 -0500

    i think i did linear control

--- motor_control/src/main.cpp
+++ motor_control/src/main.cpp
@@ -1,15 +1,76 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <Adafruit_BusIO_Register.h>
+#include <Wire.h>
+
+/*************************************************** 
+  This is an example for our Adafruit 16-channel PWM & Servo driver
+  Servo test - this will drive 8 servos, one after the other on the
+  first 8 pins of the PCA9685
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/815
+  
+  These drivers use I2C to communicate, 2 pins are required to  
+  interface.
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  100 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  475 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+// our servo # counter
+uint8_t servonum = 0;
 long previousTime;
 
 // put function declarations here:
 void linearControl(int angleResolution,int startingAngle,int finalAngle,int timePerClock);
-int myFunction(int, int);
+void write(int servo_num, int angle);
 
 void setup() {
-  // put your setup code here, to run once:
-  //int result = myFunction(2, 3);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  /*
+   * In theory the internal oscillator (clock) is 25MHz but it really isn't
+   * that precise. You can 'calibrate' this by tweaking this number until
+   * you get the PWM update frequency you're expecting!
+   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
+   * is used for calculating things like writeMicroseconds()
+   * Analog servos run at ~50 Hz updates, It is importaint to use an
+   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
+   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
+   *    the I2C PCA9685 chip you are setting the value for.
+   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
+   *    expected value (50Hz for most ESCs)
+   * Setting the value here is specific to each individual I2C PCA9685 chip and
+   * affects the calculations for the PWM update frequency. 
+   * Failure to correctly set the int.osc value will cause unexpected PWM results
+   */
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
   Serial.begin(9600);
   previousTime = millis();
 }
@@ -24,12 +85,6 @@ void loop() {
   linearControl(angleResolution, startingAngle, finalAngle, timePerClock);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  millis();
-  return x + y;
-}
-
 // a = delta theta / time in seconds
 void linearControl(int angleIncrimint, int startingAngle, int finalAngle, int timeIncrimint){
   int goalTheta = startingAngle;
@@ -38,10 +93,32 @@ void linearControl(int angleIncrimint, int startingAngle, int finalAngle, int ti
     if(((millis() - previousTime) >= timeIncrimint) ? previousTime = millis() : 0 ){
       while (startingAngle < finalAngle){
         goalTheta += angleIncrimint;
-        // write(0, goalTheta);
-
-        
+        write(0, goalTheta);        
       }
     }
   }
 }
+
+void write(int servo_num, int angle) {
+  int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(servo_num, 0, pulse);
+}
+
+
+// void loop() {
+//   // Drive each servo one at a time using setPWM()
+//   Serial.println(servonum);
+//   for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+//     pwm.setPWM(servonum, 0, pulselen);
+//     delay(10);
+//   }
+
+//   delay(500);
+//   for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+//     pwm.setPWM(servonum, 0, pulselen);
+//     delay(10);
+//   }
+
+//   delay(500);
+  
+// }
